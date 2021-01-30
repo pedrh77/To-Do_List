ac8d1d748a155f3beb179c43ba69b202e1cf918a
Pedro Santos <66256107+pedrh77@users.noreply.github.com>
2021-01-29 23:02:48 -0300

    Add files via upload

--- page/script.js
+++ page/script.js
@@ -4,8 +4,14 @@ function Click() {
     let To_do = document.createElement("li");
     To_do.textContent = document.querySelector("input").value;
     ElementP.appendChild(To_do);
-    // Deleting items 
-    To_do.addEventListener('dblclick', function Delete() {
-        ElementP.removeChild(To_do)
+    let Img = document.createElement("IMG");
+    To_do.appendChild(Img);
+    Img.src = "images/excluir.svg";
+    // Deleting items
+    Img.addEventListener('click', function Delete() {
+        var pop = prompt("Você deseja excluir este item?");
+        ElementP.removeChild(To_do);
+
     })
+
 }
